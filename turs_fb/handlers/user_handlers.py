3ee23ec8be6881628dc1a950a2d5b818d5b0ef0a
root <root@VM-887962.local>
2025-05-10 08:20:23 +0000

    добавление шагов в turs_fb

--- turs_fb/handlers/user_handlers.py
+++ turs_fb/handlers/user_handlers.py
@@ -1,14 +1,15 @@
+import asyncio
 from aiogram import F, Router
-
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import (CallbackQuery, InlineKeyboardButton,
                            InlineKeyboardMarkup, Message, PhotoSize)
-from config_data.config import user_dict, FSMFillForm
 from aiogram.filters import Command, CommandStart, StateFilter
-#from keyboards.keyboards import game_kb, yes_no_kb
+
+from keyboards.keyboards import markup_gender
 from lexicon.lexicon_ru import LEXICON_RU
+from config_data.config import user_dict, FSMFillForm
 
 router = Router()
 
@@ -39,7 +40,52 @@ async def process_cancel_inState_command(message: Message, state: FSMContext):
 @router.message(Command(commands='fillform'), StateFilter(default_state))
 async def process_fillform_command(message: Message, state: FSMContext):
   await message.answer(text=LEXICON_RU['/fillform'])
+  await asyncio.sleep(1) 
+  await message.answer(text=LEXICON_RU['name_sent'])
   await state.set_state(FSMFillForm.fill_name)
   
   
+# Этот хэндлер будет срабатывать если введено корректное Имя
+# и переводить бота в состочние ожидания ввода возраста
+@router.message(StateFilter(FSMFillForm.fill_name), F.text.isalpha())
+async def process_name_sent(message: Message, state: FSMContext):
+  # Сохраняем введенное имя в хранилище по ключу name
+  await state.update_data(name=message.text)
+  await message.answer(text=LEXICON_RU['thanks'])
+  await asyncio.sleep(1) 
+  await message.answer(text=LEXICON_RU['age_sent'])
+  await state.set_state(FSMFillForm.fill_age)
+  
+# Этот хэндлер будет срабатывать,если во время ввода
+# имени введено чтото некоректное
+@router.message(StateFilter(FSMFillForm.fill_name))
+async def warning_not_name(message: Message):
+  await message.answer(text=LEXICON_RU['wrong'])
+  await asyncio.sleep(1) 
+  await message.answer(text=LEXICON_RU['note_name'])
+
+
+# Этот хэндлер будет срабатывать, если введен коректный возраст
+# и переводить бота в состояние выбора пола
+@router.message(StateFilter(FSMFillForm.fill_age),
+                lambda x: x.text.isdigit() and 4 <= int(x.text) <= 120)
+async def process_age_sent(message: Message, state: FSMContext):
+  await state.update_data(age=message.text)
+  await message.answer(text=LEXICON_RU['thanks'])
+  await asyncio.sleep(1)
+  await message.answer(text=LEXICON_RU['gender_sent'],
+                       reply_markup=markup_gender)
+  await state.set_state(FSMFillForm.fill_gender)
+  
+# Этот хэндлер будет сратавать, если во время ввода
+# возраста ввести что нибудь некорректное
+@router.message(StateFilter(FSMFillForm.fill_age))
+async def warning_not_age(message: Message):
+  await message.answer(text=LEXICON_RU['wrong'])
+  await asyncio.sleep(1)
+  await message.answer(text=LEXICON_RU['note_age'])
+
+
+
+
 #router.message.register(process_start_command, Command(commands="start"), StateFilter(default_state))
--- turs_fb/keyboards/keyboards.py
+++ turs_fb/keyboards/keyboards.py
@@ -1,5 +1,25 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 from lexicon.lexicon_ru import LEXICON_RU
+
+
+
+male_button = InlineKeyboardButton(
+  text='Мужской ♂',
+  callback_data='male')
+
+female_button = InlineKeyboardButton(
+  text='Женский ♀',
+  callback_data='female')
+
+undefined_button = InlineKeyboardButton(
+  text='🤷 Пока не ясно',
+  callback_data='undefined_gender')
+  
+keyboard_male: list[list[InlineKeyboardButton]] = [
+  [male_button, female_button],
+  [undefined_button]]
+  
+markup_gender = InlineKeyboardMarkup(inline_keyboard=keyboard_male)
 
